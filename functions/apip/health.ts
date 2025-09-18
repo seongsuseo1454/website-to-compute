04086585db0486676a17be33718d95ede13c436d
seongsu seo <hypermedia1454@gmail.com>
2025-09-18 12:18:26 +0900

    Delete functions/

--- functions/apip/health.ts
+++
@@ -1,5 +0,0 @@
-export const onRequestGet: PagesFunction = async () => {
-  return new Response(JSON.stringify({
-    steps: 8200, heartRate: 74, bpSys: 121, bpDia: 78, sleepScore: 85, hydration: 64
-  }), { headers: { 'content-type': 'application/json' }});
-};
